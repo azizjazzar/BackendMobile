7233f96e648b0b1d1b7c04bfcc7ad22f6468ec2b
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2023-12-08 00:09:58 +0100

    Update vehicule.js

--- routes/vehicule.js
+++ routes/vehicule.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { verifyTokenMiddleware } = require("../middleware/auth");
+
 const {
   addVehicule,
   getVehiculeById,
@@ -8,7 +10,7 @@ const {
   getAllVehicules,
 } = require('../controllers/vehiculeCrud');
 
-router.post('/addvehicules', addVehicule);
+router.post('/addvehicules',verifyTokenMiddleware, addVehicule);
 router.get('/vehicules/:id', getVehiculeById);
 router.put('/vehicules/:id', updateVehicule);
 router.delete('/vehicules/:id', deleteVehicule);
