a4154368b2168d9f12d546c51cd18a6a5b645ad0
mohamedaziztemimi <80290944+mohamedaziztemimi@users.noreply.github.com>
2023-12-07 16:24:23 +0100

    Update produitCRUD.js
    show products j'ai enlevé {} dans products

--- controllers/produitCRUD.js
+++ controllers/produitCRUD.js
@@ -70,7 +70,7 @@ exports.deleteProduct = async (req, res, next) => {
 exports.getAllProducts = async (req, res, next) => {
   try {
     const products = await Product.find();
-    res.status(200).json({ products });
+    res.status(200).json(products);
   } catch (error) {
     next(error);
   }
