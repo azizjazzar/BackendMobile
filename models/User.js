497f9c312f95cecb49a0a1ea7c6965572ecc2321
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2023-12-14 20:43:13 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,87 +1,54 @@
-const crypto = require("crypto");
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
-  
   nom: {
     type: String,
-    required: true,
-    trim: true,
   },
   prenom: {
     type: String,
-    required: true,
-    trim: true,
   },
   email: {
     type: String,
-    required: true,
-    unique: false,//mbaad nbadlouha true juste l test w khaw  
-    match: [/\S+@\S+\.\S+/, "Please enter a valid email"],
   },
   genre: {
     type: String,
-    required: true,
-    enum: ["Male", "Female", "Other"],
   },
-  date_naissance: {
-    type: Date,
-    required: true,
+  datenaissance: {
+    type: String,
   },
   telephone: {
     type: String,
-    required: true,
-    trim: true,
-    match: [/^\d{10}$/, "Please enter a valid 10-digit phone number"],
   },
   adresse: {
     type: String,
-    required: true,
-    trim: true,
   },
   mot_passe: {
     type: String,
-    required: true,
-    minlength: 6,
   },
-  Type: {
+  type: {
     type: String,
-    required: true,
-    enum: ["Type1", "Type2", "Type3"],
   },
-  resetPasswordToken: String,
-  resetPasswordExpire: Date,
+  picture: {
+    type: String,
+  },
+  id: {
+    type: Number,
+    unique: true,
+  },
 });
 
-UserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    return next();
+UserSchema.pre('save', async function (next) {
+  try {
+    if (!this.id) {
+      // Générez un identifiant unique si celui-ci n'est pas déjà défini
+      const count = await mongoose.model('user').countDocuments();
+      this.id = count + 1;
+    }
+    next();
+  } catch (error) {
+    next(error);
   }
-
-  const salt = await bcrypt.genSalt(10);
-  this.mot_passe = await bcrypt.hash(this.mot_passe, salt);
-  next();
 });
 
-UserSchema.methods.matchPasswords = async function (password) {
-  return await bcrypt.compare(password, this.mot_passe);
-};
-
-UserSchema.methods.getResetPasswordToken = function () {
-  const resetToken = crypto.randomBytes(20).toString("hex");
-
-  this.resetPasswordToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
-
-  this.resetPasswordExpire = Date.now() + 10 * (60 * 1000); 
-
-  return resetToken;
-};
-
-const User = mongoose.model("User", UserSchema);
-
+const User = mongoose.model("user", UserSchema);
 module.exports = User;
