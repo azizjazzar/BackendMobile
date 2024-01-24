9a22b2f8350ee8e5d11030b8cfef9d186adccae6
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-01-24 16:00:56 +0100

    Structure commit

--- server.js
+++ server.js
@@ -7,28 +7,15 @@ const app = express();
 const stripe = require("stripe")('sk_test_51OErmACis87pjNWpHjxy4jOfBeV5X2cD3bB2op5qNVdo8OY7pqpqJh235cFlSwbjNxfjsz6FMZAD1EVCWJs2kyDq00LYDaUrax');
 connectDB();
 app.use(express.json());
-
+app.set('views','./views')
+app.set('view engine','ejs')
 
 app.get("/", (req, res) => {
-  res.send("Welcome to our server Electrigo !");
+  res.render("home")
 });
 
 app.use("/api/auth", require("./routes/auth"));
 
-app.use("/api/borne", require("./routes/borne"));
-app.use("/api/review", require("./routes/review"));
-app.use("/api/garage", require("./routes/garage"));
-app.use("/api/posts", require("./routes/postRoutes")); 
-app.use("/api/product", require("./routes/product"));
-app.use("/api/favories", require("./routes/FavoritesRoutes"));
-app.use('/api/carte',  require("./routes/CarteRoutes"));
-app.use('/api/rating',  require("./routes/ratingRoutes"));
-app.use("/api/categorie", require("./routes/categorie")); // Use consistent naming
-app.use("/api/vehicule", require("./routes/vehicule"));
-app.use("/api/reservation", require("./routes/reservation"));
-app.use("/api/payment", require("./routes/payment"));
-app.use("/api/mobile", require("./routes/Send"));
-app.use("/api/posts", require("./routes/postRoutes"));
 
 
 app.listen(3000, '0.0.0.0', function() {
